a90cb79d0c6294f2b7854091bb0f9b61f31b5381
Marc Held <heldgeek@gmail.com>
2013-09-01 23:03:32 -0700

    forgot about the port number

--- activerecord-maxdb-adapter/lib/arjdbc/maxdb/connection_methods.rb
+++ activerecord-maxdb-adapter/lib/arjdbc/maxdb/connection_methods.rb
@@ -2,7 +2,7 @@ class ActiveRecord::Base
   class << self
     def maxdb_connection( config )
       config[:port] ||= 30015
-      config[:url] ||= "jdbc:sap://#{config[:host]}/#{ config[:database]}"
+      config[:url] ||= "jdbc:sap://#{config[:host]}:#{config[:port]}/#{ config[:database]}"
       config[:driver] ||= "com.sap.db.jdbc.Driver"
       config[:adapter_class] = ActiveRecord::ConnectionAdapters::MaxDBSQLAdapter
       jdbc_connection(config)
